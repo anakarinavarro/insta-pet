2f09acdbf5f26701f07532c5b8c573720f5707f3
Daniel Rebolledo <43189313+DanielRebolledo@users.noreply.github.com>
2022-07-16 20:16:55 -0400

    home,footer,error_404r

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="d-flex align-items-center justify-content-center vh-100">
-    <div class="text-center">
-      <h1 class="display-1 fw-bold">404</h1>
-      <p class="fs-3"><span class="text-danger">Opps!</span> Pagina no encontrada.</p>
-      <p class="lead">Por favor revisa bien la url.</p>
-      <router-link to="/" class="btn btn-primary">Volver al home</router-link>
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <img src="../../public/assets/img/error_404.png" alt="error_404_img" class="error_404" />
+        <h1 class="fs-3 white--text">
+          Opps! <br />
+          Pagina no encontrada
+        </h1>
+        <h2 class="lead white--text mb-10">Por favor revisa bien la url</h2>
+        <router-link to="/" class="btn-pink white--text">Volver al home</router-link>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+.error_404 {
+  width: 100%;
+  max-width: 700px;
+}
+.btn-pink {
+  background-color: #fc5374;
+  text-decoration: none;
+  padding: 10px 20px;
+  border-radius: 8px;
+  margin: 40px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,28 @@
 <template>
   <v-container style="background-color: #7400fc">
-    <v-row>
-      <v-col class="sm-6">
-        <v-layout class="mt-5" justify-space-around align-center style="height: 600px">
-          <v-flex xs12>
-            <img src="../../public/assets/img/InstaPet_logo.svg" alt="" style="width: 150px" />
-            <h2>Encuentra a tu</h2>
-            <h2 class="text-lighten-5">amigo ideal</h2>
-            <v-btn>Registrate</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-col>
-      <v-col class="sm-6">
-        <v-layout class="mt-5" justify-space-around align-center>
+    <v-layout row style="align-items: center">
+      <v-flex xs12 sm6>
+        <v-card class="transparent d-flex justify-center mb-6" elevation="0">
+          <v-card class="transparent" elevation="0">
+            <h2 class="home-title">Encuentra a</h2>
+            <h2 class="home-title"><span>tu amigo ideal</span></h2>
+            <br />
+            <v-btn color="#fc5374" class="home-btn white--text">Registrate</v-btn>
+          </v-card>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-card class="transparent" elevation="0">
           <v-img
             width="100%"
+            min-width="3"
             max-width="600"
             src="../../public/assets/img/home_img.png"
             position="center"
           ></v-img>
-        </v-layout>
-      </v-col>
-    </v-row>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -33,5 +34,12 @@ export default {}
 .v-application--wrap {
   font-family: 'Montserrat', sans-serif;
   background-color: #7400fc;
+}
+.home-title {
+  color: #ffffff;
+  font-size: 50px;
+}
+.home-title span {
+  color: #ffbd07;
 }
 </style>
